64204fe981a6a5dfc9209a1782ff4e772db63d83
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-27 00:22:18 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  
+  def self.all()
+    @@all
+  end
+  
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+  
+  def songs()
+    @songs
+  end
+  
+end
